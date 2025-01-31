251021d8efea18bd4b43c21530e2ab75d32f225c
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-01-31 16:16:09 +0530

    arithmatic operator

---
+++ src/Arithmetic_Operators.java
@@ -0,0 +1,14 @@
+public class Arithmetic_Operators {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        System.out.println("-------Operations-------");
+
+        System.out.println("Addition: "+(a+b));
+        System.out.println("Substraction: "+(a-b));
+        System.out.println("Multiplication: "+(a*b));
+        System.out.print("Modulus: "+(a%b));
+
+    }
+}
