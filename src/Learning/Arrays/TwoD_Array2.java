3de0d87bc32053ba706fb8cea3d2e22ef8a7b39c
Kunal Sunil Koli <kunalkoli537@gmail.com>
2025-02-05 21:01:47 +0530

    2D Array

---
+++ src/Learning/Arrays/TwoD_Array2.java
@@ -0,0 +1,14 @@
+package Learning.Arrays;
+
+public class TwoD_Array2 {
+    public static void main(String[] args) {
+        int [] [] myArray={{1,2,3},{4,5,6}};
+        System.out.println(myArray[0][0]);
+        System.out.println(myArray[0][1]);
+        System.out.println(myArray[0][2]);
+        System.out.println(myArray[1][0]);
+        System.out.println(myArray[1][1]);
+        System.out.println(myArray[1][2]);
+
+    }
+}
